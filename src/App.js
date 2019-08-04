e07b0fa0dc4f990fbb221934b550a3808239f841
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-04 19:48:09 -0300

    NEW - Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [techs, setTechs] = useState([]);
@@ -21,6 +21,8 @@ function App() {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
 
+  const techsSize = useMemo(() => techs.length, [techs]);
+
   return (
     <>
       <ul>
@@ -28,6 +30,8 @@ function App() {
           <li key={tech}>{tech}</li>
         ))}
       </ul>
+      <strong>Você tem {techsSize} tecnologias</strong>
+      <br />
       <input
         type="text"
         value={newTech}
