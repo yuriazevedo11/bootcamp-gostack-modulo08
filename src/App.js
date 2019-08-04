2a75f9727f5e3fb350012ae08b67a1c918c46417
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-04 19:28:04 -0300

    NEW - Hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [newTech, setnewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setnewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setnewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
